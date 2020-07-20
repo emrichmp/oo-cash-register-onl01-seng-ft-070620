e58532aa56524d5f61f6c92853f63f46d32f32fd
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-20 02:21:41 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,9 +1,10 @@
 class CashRegister
   
-  attr_accessor :total :discount 
+  attr_accessor :total :discount :quantity :price
   
   def initialize(discount = 0)
     @total = total
     @discount = discount
+    
   end
 end
