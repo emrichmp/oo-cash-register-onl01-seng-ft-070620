862ad302af6459612cbaa99e6e1bf984ba89fc5d
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-20 02:33:08 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -25,7 +25,7 @@ class CashRegister
     if discount == 0
       puts "There is no discount to apply"
     else
-      self.total = @total - (0.01*)
+      self.total = @total - (0.01*@discount*@total)
     end
   end
 end
